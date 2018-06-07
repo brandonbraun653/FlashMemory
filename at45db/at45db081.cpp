be090d1b9816ea3c59a336ef9ca793ab97864be7
Brandon Braun <brandonbraun653@gmail.com>
2018-06-07 12:09:05 -0500

    Able to read device id

--- at45db/at45db081.cpp
+++ at45db/at45db081.cpp
@@ -1,6 +1,41 @@
+/* Boost Includes */
+#include <boost/make_shared.hpp>
+
 #include "at45db081.hpp"
 
-namespace AT45DBFlash
+using namespace Chimera::SPI;
+
+namespace Adesto
 {
-  //Do Stuff
+	namespace NORFlash
+	{
+		AT45::AT45(const int& spiChannel)
+		{
+			spi = boost::make_shared<SPIClass>(spiChannel);
+
+			/* These settings are hard coded for reliable communications. The user will
+			 * only be able to control which spi channel is used and its clock frequency. */
+			setup.clockFrequency = 1000000;
+			setup.bitOrder = MSB_FIRST;
+			setup.clockMode = MODE0;
+			setup.dataSize = DATASIZE_8BIT;
+			setup.mode = MASTER;
+		}
+
+		bool AT45::initialize(uint32_t clockFreq)
+		{
+			if (spi->begin(setup) != SPI_OK)
+				return false;
+			
+			
+			uint8_t OPdevID[5] = { 0x9f, 0x00, 0x00, 0x00, 0x00 };
+			uint8_t data[5] = { 0x00, 0x00, 0x00, 0x00, 0x00 };
+			
+			spi->write(OPdevID, data, 5);
+			
+			memset(data, 0, 5);
+			
+			return true;
+		}
+	}
 }
--- at45db/at45db081.hpp
+++ at45db/at45db081.hpp
@@ -5,22 +5,45 @@
 /* Standard C++ Includes */
 #include <stdlib.h>
 
+/* Boost Includes */
+#include <boost/shared_ptr.hpp>
+
+/* Chimera Includes */
+#include <Chimera/spi.hpp>
+
+#if defined(USING_FREERTOS)
+#include <Chimera/threading.hpp>
+#endif
+
 /* Supporting Includes */
 #include "at45db081_definitions.hpp"
 
-namespace AT45DBFlash
+namespace Adesto
 {
-	class AT45DB
+	namespace NORFlash
 	{
-	public:
+		class AT45
+		{
+		public:
+			static const size_t numSectors = 16;
+			static const size_t numBlocks = 512;
+			static const size_t numPages = 4096;
+			static const size_t numBytesPerPage = 256;
 
+			AT45(const int& spiChannel);
+			~AT45() = default;
 
-		AT45DB() = default;
-		~AT45DB() = default;
+			//Need options to to run at desired clock frequency
 
-	private:
+			bool initialize(uint32_t clockFreq);
 
-	};
+			
 
+		private:
+			Chimera::SPI::SPIClass_sPtr spi;	/**< SPI object used for talking with the flash chip */
+			Chimera::SPI::Setup setup;			/**< SPI initialization settings */
+		};
+		typedef boost::shared_ptr<AT45> AT45_sPtr;
+	}
 }
 #endif /* AT45DB081_HPP */
